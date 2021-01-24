243c0dea0786f5cc8b1649973c33f3ba8ef03776
bestcoderzoo <best.coder.jiwoo@gmail.com>
2021-01-24 23:08:24 +0900

    Add comments

--- First.java
+++ First.java
@@ -1,6 +1,6 @@
-public class First {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+public class First { // Hello의 H가 대문자로 작성되어야함. (파일명과 대소문자가 같아야함)
+    public static void main(String[] args) { //String은 'S' 가 대문자
+        System.out.println("Hello"); //System 'S'대문자 세미콜론 잊지 않기.
     }
     
 }
