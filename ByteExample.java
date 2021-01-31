59462f3b9c67bd864e7d80fddb231b357cb49c23
bestcoderzoo <best.coder.jiwoo@gmail.com>
2021-02-01 00:58:54 +0900

    commit

--- ByteExample.java
+++ ByteExample.java
@@ -3,7 +3,7 @@ public class ByteExample {
         byte var1 = -128;
         byte var2 = -30;
         byte var3 = 0;
-        byte var4 = 30;
+        byte var4 = 30; 
         byte var5 = 127;
         //byte var6 = 128; // 컴파일 에러 byte일 경우 -128 최소값부터 시작해서 127최대값을 넘으면 다시 -128부터 시작하게 된다. 
         // 엉터리값 = 쓰레기값. 
--- CharExample.java
+++ CharExample.java
@@ -3,7 +3,7 @@ public class CharExample {
         char c1 = 'A'; //문자를 직접 저장
         char c2 = 65; //10진수로 저장
         char c3 = '\u0041'; //16진수로 저장
-
+ 
         char c4 = '가'; //문자를 직접 저장
         char c5 = 44032; //10진수로 저장
         char c6 = '\uac00'; //16진수로 저장
